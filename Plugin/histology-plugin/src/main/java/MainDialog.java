1ab81b1dfafb32eef9047f5bd64c401f98a0555b
Stefano Belli <Luxor001@gmail.com>
2019-03-25 21:12:07 +0100

    Forzata l'esecuzione dei garbage collectors per risolvere il problema di memory leaks all'interno dell'applicazione

--- Plugin/histology-plugin/src/main/java/MainDialog.java
+++ Plugin/histology-plugin/src/main/java/MainDialog.java
@@ -43,6 +43,7 @@ public class MainDialog {
                 imp.close();
                 imp = new ImagePlus("", reader.openImage(currentImage));
                 imp.show();
+                System.gc();
             } catch (Exception e1) {
                 e1.printStackTrace();
             }
@@ -54,6 +55,7 @@ public class MainDialog {
                 imp.close();
                 imp = new ImagePlus("", reader.openImage(currentImage));
                 imp.show();
+                System.gc();
             } catch (Exception e1) {
                 e1.printStackTrace();
             }
