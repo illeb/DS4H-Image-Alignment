98d3e40d491866ff64ecf5784b2ca46188b15d58
DESKTOP-RABL8RP\luxor <luxor001@gmail.com>
2021-03-08 19:48:29 +0100

    Release 1.0.6

--- Plugin/DS4H-Image-Alignment/src/main/java/DS4H/AboutDialog.java
+++ Plugin/DS4H-Image-Alignment/src/main/java/DS4H/AboutDialog.java
@@ -217,7 +217,7 @@ public class AboutDialog extends JDialog {
         lbl_version = new JLabel();
         lbl_version.setAlignmentX(0.0f);
         lbl_version.setAlignmentY(1.0f);
-        lbl_version.setText("v. 1.0.5 2009");
+        lbl_version.setText("v. 1.0.6 2103");
         gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 1;
